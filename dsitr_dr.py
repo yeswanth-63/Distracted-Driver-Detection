2868c5142895e5cc2c5ea32850ee668d2d396ce1
yeswanth-63 <113228720+yeswanth-63@users.noreply.github.com>
2023-07-02 21:33:54 +0530

    app.py

--- dsitr_dr.py
+++ dsitr_dr.py
@@ -1,55 +1,43 @@
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
-import pickle
-model = pickle.load(open('driver_distraction_model.pkl',"rb"))
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image as kimage
-def preprocess(image):
-    # Resize the image to the input shape expected by your model
-    image = image.resize((100, 100))
+import numpy as np
 
-    # Convert the image to an array of pixel values
-    image_array = kimage.img_to_array(image)
+# Load the pre-trained model from the .h5 file
+model = tf.keras.models.load_model('distracted_driver.h5')
 
-    # Normalize the pixel values (assuming your model expects values in the range [0, 1])
-    image_array = image_array / 255.0
+# Define the class labels
+class_labels = ['safe driving', 'texting - right', 'talking on the phone - right', 'texting - left',
+                'talking on the phone - left', 'operating the radio', 'drinking', 'reaching behind',
+                'hair and makeup', 'talking to passenger']
 
-    # Expand the dimensions of the image to match the input shape expected by your model
-    image_array = tf.expand_dims(image_array, 0)
+# Define the Streamlit app layout
+st.title('Distracted Driver Detection')
+st.write('Upload an image of a driver to predict the distraction category')
 
-    return image_array
+# Create a file upload button
+uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
 
+# Function to preprocess the image
+def preprocess_image(image):
+    image = image.resize((128, 128))  # Resize the image to match the input size of the model
+    image = np.array(image)  # Convert the PIL image to a NumPy array
+    image = image / 255.0  # Normalize the image
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    return image
 
+# Function to make predictions
 def predict(image):
-    # Preprocess the image (e.g., resize, normalize, etc.)
-    processed_image = preprocess(image)
+    processed_image = preprocess_image(image)
+    prediction = model.predict(processed_image)[0]
+    predicted_class = np.argmax(prediction)
+    confidence = prediction[predicted_class]
+    return class_labels[predicted_class], confidence
 
-    # Make predictions using the loaded model
-    predictions = model.predict(processed_image)
-
-    return predictions
-
-def main():
-    st.title("Distracted Driver Detection")
-    st.write("Upload an image of a driver and get the predicted class.")
-
-    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-
-    if uploaded_file is not None:
-        # Read the uploaded image
-        image = Image.open(uploaded_file)
-
-        # Display the uploaded image
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-
-        # Perform prediction
-        predictions = predict(image)
-
-        # Display the predicted class and probabilities
-        class_names = ['C0', 'C1', 'C2','C3', 'C4', 'C5','C6', 'C7', 'C8','C9']  # Replace with your actual class names
-        st.write("Predicted class:", class_names[predictions.argmax()])
-        st.write("Class probabilities:", predictions)
-
-if __name__ == '__main__':
-      main()
+# Make predictions if an image is uploaded
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    predicted_class, confidence = predict(image)
+    st.write(f'Predicted Class: {predicted_class}')
+    st.write(f'Confidence: {confidence}')
